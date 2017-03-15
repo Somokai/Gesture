1618413a9ad44c9d93358d657fe9c985a930ff6f
Zac Long <longzc@mail.uc.edu>
2017-03-15 10:53:22 -0400

    Removed some files, added comments to main app

--- Gesture.py
+++ Gesture.py
@@ -7,15 +7,18 @@ import time
 
 #time.sleep(5)
 
+#Sets the cursor position to a certain point
 def point(x,y):
     win32api.SetCursorPos((x,y))
-
+    
+#Finds the center of the tracking box
 def average(pts):
     avg = []
     avg.append((pts[1][0]+pts[3][0])/2)
     avg.append((pts[1][1]+pts[3][1])/2)
     return(avg)
 
+#I think this checks to see the standard deviation of tm
 def check(tm):
     x = []
     y = [] 
@@ -24,12 +27,32 @@ def check(tm):
         y.append(item[1])
     return [np.std(x), np.std(y)]
  
+#This checks to see if the object is inside of the borders of the projected image
 def checkClick(pointer, border):
     #print(border)
     #print(pointer)
     if pointer[0][1] > border[0] and pointer[0] [1] < border[1] and pointer [0][0] > border [2] and  pointer[0][0] < border[3]:
         return(1)
 
+
+#Function for getting sides
+def cornerCount(tot, start, val):
+    i = 0
+    init = np.sum(image[start][np.abs(i-val)])
+    for x in range(int(tot)):
+        p_array = image[start][np.abs(i-val)]
+        #print(p_array)
+        #if p_array[1] - p_array[0] > 20 and p_array[1] - p_array[2] > 85:
+        sum = int(np.sum(p_array))
+        #print(p_array)
+        #print(np.sum(image[start][np.abs(i-val+1)]))
+        if np.abs(init - sum) > 150:
+	        i += 1
+	        loc.append(np.abs(i-val))
+	        break
+        else:
+	        i+=1
+            
 cap = cv2.VideoCapture(0)
 cam = cv2.VideoCapture(2)
 s, im = cam.read() # captures image
@@ -52,24 +75,6 @@ print(height)
 
 loc=[]
 
-#function for getting sides
-def cornerCount(tot, start, val):
-    i = 0
-    init = np.sum(image[start][np.abs(i-val)])
-    for x in range(int(tot)):
-        p_array = image[start][np.abs(i-val)]
-        #print(p_array)
-        #if p_array[1] - p_array[0] > 20 and p_array[1] - p_array[2] > 85:
-        sum = int(np.sum(p_array))
-        #print(p_array)
-        #print(np.sum(image[start][np.abs(i-val+1)]))
-        if np.abs(init - sum) > 150:
-	        i += 1
-	        loc.append(np.abs(i-val))
-	        break
-        else:
-	        i+=1
-
 #getting four corners
 cornerCount(length*2 - 1, height, 0)
 cornerCount(length*2 - 1, height, length*2 - 1)
@@ -89,6 +94,8 @@ roi = frame[r:r+h, c:c+w]
 hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv_roi, np.array((110., 30., 30.)), np.array((130.,200.,200.)))
 #mask2 = cv2.inRange(hsv_roi, np.array((110., 50.,50.)), np.array((130.,255.,255.)))
+
+#this is finding the box for tracking, I don't like the way it works though so it needs fixed
 roi_hist1 = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
 roi_hist2 = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
 cv2.normalize(roi_hist1,roi_hist1,0,255,cv2.NORM_MINMAX)
@@ -109,7 +116,7 @@ while(1):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         dst1 = cv2.calcBackProject([hsv],[0],roi_hist1,[0,180],1)
         dst2 = cv2.calcBackProject([hsv],[0],roi_hist2,[0,180],1)
-        # apply meanshift to get the new location
+        #Apply meanshift to get the new location
         ret1, track_window1 = cv2.CamShift(dst1, track_window1, term_crit)
         ret2, track_window2 = cv2.CamShift(dst2, track_window2, term_crit)
         # Draw it on image
@@ -127,7 +134,7 @@ while(1):
             avg2 = average(pts2*3)
             tm.append(avg2)
             if avg1[0]!=0:
-                if 1 == 1: #
+                if 1 == 1: 
                     print(timei)
                     print(timp)
                     deltat = timei-timp
@@ -141,6 +148,7 @@ while(1):
                         timei = timi()
                         x = int(chk[0])
                         y = int(chk[1])
+                        #clicks the mouse
                         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
                         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
                         if chk[0] < 6 and chk[1] < 6:
